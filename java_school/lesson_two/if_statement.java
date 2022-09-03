cd92ccb05e73dbf01a30a73bb9a4f6d06a0bfee4
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-09-03 02:03:42 -0500

    Update if_statement.java

--- java_school/lesson_two/if_statement.java
+++ java_school/lesson_two/if_statement.java
@@ -3,3 +3,17 @@ boolean isCold = true;
 if(isCold) {
   System.out.println("It's cold, wear a coat!");
 }
+
+///
+boolean isLightGreen = true
+boolean isLightYellow = false;
+
+if(isLightGreen) {
+  // traffic light is on green
+  System.out.println("Drive");
+} else if(isLightYellow){
+  // light is not green but it is yellow
+  System.out.println("Slow Down"); 
+} else {
+  // light is nther green nor yellow
+  System.out.println("Stop.");} 
