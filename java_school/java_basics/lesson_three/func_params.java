5b134431f87a7767fe9fcf8174eceeebd16bac76
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-09-14 00:28:17 -0500

    Update func_params.java

--- java_school/java_basics/lesson_three/func_params.java
+++ java_school/java_basics/lesson_three/func_params.java
@@ -1,8 +1,22 @@
 // Parameters and Arguments
 
+// single paramter
 public void greeting(String location) {
   // greet a specific location
   System.out.println("Hello, " + location);
 }
 
 greeting("Salt Lake");
+
+
+// multiple parameter
+public void printPhoto(int width, int height, boolean inColor) {
+  System.out.println("Width = " + width + " cm");
+  System.out.println("Height = " + height + " cm");
+  if(inColor) {
+    System.out.println("Print in full color.")
+  } else {
+    System.out.println("Print in black and white.")
+  }
+}
+printPhoto(30, 40, true);
