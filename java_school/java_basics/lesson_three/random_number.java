05c5707c415adb3e9be085da1481b4d12d189038
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-09-18 00:03:08 -0500

    Update random_number.java

--- java_school/java_basics/lesson_three/random_number.java
+++ java_school/java_basics/lesson_three/random_number.java
@@ -5,3 +5,7 @@ double randomNumber = Math.random();
 
 // change range to 0 to (almost) 10
 randomNumber = randomNumber * 10;
+
+// cast to integer (ignore decimal part)
+// ex. 9.985 becomes 9
+int randomInt = (int) randomNumber;
