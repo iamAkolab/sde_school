e1a2796ada03d61d5d2bd6909f94b4f570b7d1cc
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-09-24 00:24:37 -0500

    Update java_documentation.java

--- java_school/java_basics/lesson_three/java_documentation.java
+++ java_school/java_basics/lesson_three/java_documentation.java
@@ -5,3 +5,15 @@ Multiline Comeents
 Hi, I'm a multiline comment
 Good for longer descriptions
 */
+
+
+// ---------------------------------------------------------------
+// Java documentation (doc) comments
+/** 
+* General descrition of a function.
+* 
+* @param a first input parameter, named a
+* @param b second parameter, named b
+*
+* @return description of return value
+*/
