1fa1b45ff33ecb0676fc38ca18395d4594206a12
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-09-23 23:59:38 -0500

    Update rolling_dice.java

--- java_school/java_basics/lesson_three/rolling_dice.java
+++ java_school/java_basics/lesson_three/rolling_dice.java
@@ -3,4 +3,14 @@
 public int rollDice() {
   // random num between 0 and (almost) 1
   double randomNumber = Math.random();
+  
+  // change range to 0 to (almost) 6
+  randomNumber = randomNumber *6;
+  
+  // shift range up one
+  randomNumber = randomNumber + 1;
+  
+  // cast to integer (ignore decimal part)
+  // ex. 6.998 becomes 6
+  nt randomInt = (int) randomNumber;
 }
