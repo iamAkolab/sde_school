39593628b7605a187fe657193eed14fc58879cb4
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-10-05 01:20:56 -0500

    Create alarm.java

---
+++ java_school/java_basics/lesson_four/alarm.java
@@ -0,0 +1,14 @@
+/**
+* makes a single beep sound
+*/
+public void beep()
+  
+/**
+* @return true if alaram is on, false if off
+*/
+  public boolean checkAlarm()
+  
+ /**
+* keep beeping until snoozed
+*/
+  public void alarm()
