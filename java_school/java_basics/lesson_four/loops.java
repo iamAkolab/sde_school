6b24d50ddf9ada259dd7a072a1a1d744af77fcec
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-10-19 00:36:10 -0500

    Create loops.java

---
+++ java_school/java_basics/lesson_four/loops.java
@@ -0,0 +1,16 @@
+// while loop
+public void raiseAlarm(int numOfWarings) {
+  int i = 1;
+  
+  while (i <= numOfWarnings) {
+    System.out.println("Warning");
+    i++;
+  }
+}
+
+// for loop
+public void raiseAlarm(int numOfWarings) {
+  for(int i=1; i<= numOfWarnings; i++) {
+    System.out.println("Warning");
+  }
+}
