ac749811e524964e53772853d43658e04e7d472c
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-10-20 01:09:44 -0500

    Update rollASix.java

--- java_school/java_basics/lesson_four/rollASix.java
+++ java_school/java_basics/lesson_four/rollASix.java
@@ -2,6 +2,24 @@
 * Rolss a dice till you get a 6 then you win 
 * but if you get a 3 you lose
 */
+
+public int rollDice(int sides) {
+  // random num between 0 and (almost) 1
+  double randomNumber = Math.random();
+  
+  // change range to 0 to (almost) 6
+  randomNumber = randomNumber * sides;
+  
+  // shift range up one
+  randomNumber = randomNumber + 1;
+  
+  // cast to integer (ignore decimal part)
+  // ex. 6.998 becomes 6
+  int randomInt = (int) randomNumber;
+  
+  // returm statement
+  return randomInt;
+}
 public boolean rollASix() {
   int dice = rollDice();
   while(dice != 6) {
