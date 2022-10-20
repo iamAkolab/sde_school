20b6ac8686417070bc250fdd72b5ba4a1479ca7a
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-10-20 02:19:15 -0500

    Update loop2Darrays.java

--- java_school/java_basics/lesson_four/loop2Darrays.java
+++ java_school/java_basics/lesson_four/loop2Darrays.java
@@ -1,1 +1,9 @@
-
+public static double englishAverage(int [][] grades) {
+  int total = 0;
+  
+  for(int i= 1; i < 5; i++){
+    total += grades[1][i];
+  }
+  double average = total/5.0;
+  return average;
+}
