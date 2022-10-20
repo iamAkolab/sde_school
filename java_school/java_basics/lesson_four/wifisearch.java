7a13cb42fcae27655f418e0710671a23554a72d9
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2022-10-20 00:36:27 -0500

    Create wifisearch.java

---
+++ java_school/java_basics/lesson_four/wifisearch.java
@@ -0,0 +1,13 @@
+/**
+* WiFi Search
+*/
+
+public void searchWifi() {
+  for (int i = 1; i <= 10; i++) {
+  boolean wifiAvailable = checkWifi();
+  if (wifiAvailable){
+    System.out.println("Wifi found");
+    }
+  }
+
+}
